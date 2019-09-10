381b3e61e8f3efb4c6f73064c4faad978924ecc7
Konrad Pabjan <Konrad.Pabjan@microsoft.com>
2019-09-10 16:36:16 -0400

    Cleanup

--- index.js
+++ index.js
@@ -4,160 +4,134 @@ const graphql = require('@octokit/graphql');
 
 async function run() {
     const myToken = core.getInput('repo-token');
-    const projectId = core.getInput('project-id');
+    const repositoryOwner = core.getInput('repository-owner');
+    const repositoryName = core.getInput('repository-name');
+    const projectId = core.getInput("project-id");
     const columnId = core.getInput('column-id');
     const labelName = core.getInput('label-name');
     const octokit = new github.GitHub(myToken);
     const context = github.context;
 
-    console.log(context.payload.issue.labels);
-
     var found = false;
     context.payload.issue.labels.forEach(function(item){
-        console.log(item.name)
         if(labelName == item.name){
-            // the label matches
-            console.log("the label matches: " + labelName)
             found = true;
         }
     })
 
     if(found){
-        try{
-            // This might fail since the card is already created?
-            /*
-            await octokit.projects.createCard({
-                column_id: columnId,
-                content_id: context.payload.issue.id,
-                content_type: "Issue"
-            });cls
-            */
+        var isCardCreated = tryCreateCard(octokit, columnId, context.payload.issue.id);   
+        if (!isCardCreated){
+            // A card already exists, we must find the cardId and move it to the correct column
+            var cardId = findCardId(myToken, projectId, repositoryOwner, repositoryName);
+            if(cardId){
+                moveCard(octokit, cardId, columnId);
+                return `Sucesfully moved card #${cardId} to column #${columnId}`;
+            } else {
+                // unable to find the card?
+                return `#ERROR# Unable to find a card with an id of #${context.payload.issue.id}, not doing anything`;
+            }
+        } else {
+            // A card was created in the column that we want it to end up in
+            return `A card was created in column #${columnId} for issue #${context.payload.issue.id}`;
+        }
+    } else {
+        // None of the labels match what we are looking for
+        return `Issue #${context.payload.issue.id} does not have a label that matches ${labelName}, ignoring`;
+    }
+}
 
-            // for now assume that a card already exists for the issues
-            // query for all of the cards in the project and get the card id of the issue
+async function tryCreateCard(octokit, columnId, issueId){
+    try {
+        await octokit.projects.createCard({
+            column_id: columnId,
+            content_id: issueId,
+            content_type: "Issue"
+        });
+        return true;
+    } catch (error) {
+        // the card already exists in the project
+        return false;
+    }
+}
 
-            console.log("we are going to be looking for the card with ID#:  " +context.payload.issue.id);
+async function moveCard(octokit, cardId, columnId){
+    await octokit.projects.moveCard({
+        card_id: cardId,
+        position: "top",
+        column_id: columnId
+    })
+}
 
-           console.log("runing graphQL query to find all of the cards in a project");
-           // graphQL query, very good to play around with https://developer.github.com/v4/explorer/
-           const response  = await graphql(
-               `
-               {
-                repository(owner: "bbq-beets", name: "konradpabjan-test") {
-                  projects(first: 100) {
-                    nodes {
-                      databaseId
-                      columns(first: 100) {
-                        edges {
-                          node {
-                            databaseId
-                            name
-                            cards {
-                              edges {
-                                node {
-                                  databaseId
-                                  content {
-                                    ... on Issue {
-                                      databaseId
-                                      number
-                                    }
-                                  }
-                                }
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-               `,
-               {
-                 headers: {
-                   Authorization: `bearer ${myToken}`
+async function findCardId(token, projectId, repositoryOwner, repositoryName){
+    // GraphQL query to get all of the cards in each column for a project
+    // https://developer.github.com/v4/explorer/ is good to play around with 
+    const response  = await graphql(
+    `
+        {
+         repository(owner: ${repositoryOwner}, name: ${repositoryName}) {
+           projects(first: 100) {
+             nodes {
+               databaseId
+               columns(first: 100) {
+                 edges {
+                   node {
+                     databaseId
+                     name
+                     cards {
+                       edges {
+                         node {
+                           databaseId
+                           content {
+                             ... on Issue {
+                               databaseId
+                               number
+                             }
+                           }
+                         }
+                       }
+                     }
+                   }
                  }
                }
-             );
-
-            console.log(response);
-
-            var cardId = null;
-
-            response.repository.projects.nodes.forEach(function(project){
-                // we are at the project level
-                console.log(project.columns)
-                project.columns.edges.forEach(function(column){
-                    // column level
-                    console.log(column);
-                    column.node.cards.edges.forEach(function(card){
-                        // card level
-                        console.log(card);
-
-                        // check if the issue databaseId matches the databaseId of the card content
-                        if (card.node.content != null){
-                            // only issues and pull requests have content
-                            if (card.node.content.databaseId == context.payload.issue.id){
-                                console.log("We have a match!!");
-                                cardId = card.node.databaseId;
-                            }
-                        }
-                    });
-                });
-            })
-
-            console.log("Done searching for the card id");
-            console.log("card id is: " + cardId);
-
-            // gonna try to move the card to the super important column
-            await octokit.projects.moveCard({
-                card_id: cardId,
-                position: "top",
-                column_id: 6443965
-            })
-
-            /*
-            console.log("runing graphQL query #2");
-            const response2  = await graphql(
-                `
-                  {
-                    repository(owner:"konradpabjan", name:"Testing2") {
-                        issues(states:CLOSED) {
-                          totalCount
-                        }
-                      }
-                  }
-                `,
-                {
-                  headers: {
-                    Authorization: `bearer ${myToken}`
-                  }
-                }
-              );
-              */
-
-            //console.log(response2);
-            
-        } catch (error) {
-            console.log(error)
-            /*
-            // fetch all of the columns for the project
-            var columnInformation = await octokit.projects.listColumns({
-                project_id: 3181121
-            });
-            console.log(columnInformation)
-            // we're going to have to get all the columns in 
-            columnInformation.data.forEach(function )
-            */
+             }
+           }
+         }
+       }
+    `,
+    {
+        headers: {
+        authorization: `bearer ${token}`
         }
     }
+    );
 
-    return "Initial Testing";
+    var cardId = null;
+    response.repository.projects.nodes.forEach(function(project){
+        // project level
+        // make sure the projectId is correct, the same issue can be in multiple projects
+        if (project.databaseId == projectId){
+            project.columns.edges.forEach(function(column){
+                // column level
+                column.node.cards.edges.forEach(function(card){
+                    // card level
+                    // check if the issue databaseId matches the databaseId of the card content
+                    if (card.node.content != null){
+                        // only issues and pull requests have content
+                        if (card.node.content.databaseId == context.payload.issue.id){
+                            cardId = card.node.databaseId;
+                        }
+                    }
+                });
+            });
+        }
+    })
+    return cardId;
 }
 
 run()
     .then(
-        (testing) => { console.log(`Testing # ${testing}`) },
+        (response) => { console.log(`Finished running: ${response}`) },
         (err)  => { console.log(err) }
     )
     .then(
